d6f2160f0113ab8dd118ecd0faa2b7f1c93c9b64
USER <USER@DESKTOP-MVFTAB2>
2024-01-15 11:35:37 +0900

    0115/11:34/고채린/main

--- Ex17TeamGit/src/view/Main.java
+++ Ex17TeamGit/src/view/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("무언가를 출력해주는 main입니다");
+		System.out.println("형태만 만들어 보겠습니다 ");
+		System.out.println("프로젝트 생성");
 
 	}
 
